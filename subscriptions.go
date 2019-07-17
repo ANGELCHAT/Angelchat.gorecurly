5a0b3f7b3292b77b3af8c70cf07d6606d1edfe8f
Łukasz Wojciechowski <nostrzak@gmail.com>
2019-07-17 08:10:57 +0200

    Omit empty unit price for addons

--- subscriptions.go
+++ subscriptions.go
@@ -155,7 +155,7 @@ type SubscriptionAddOn struct {
 	XMLName           xml.Name `xml:"subscription_add_on"`
 	Type              string   `xml:"add_on_type,omitempty"`
 	Code              string   `xml:"add_on_code"`
-	UnitAmountInCents int      `xml:"unit_amount_in_cents"`
+	UnitAmountInCents int      `xml:"unit_amount_in_cents,omitempty"`
 	Quantity          int      `xml:"quantity,omitempty"`
 }
 
